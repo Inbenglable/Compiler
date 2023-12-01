c7a30e1e043cdabceb11cae5bda496bd88f82841
radiance0718 <3362985694@qq.com>
2023-12-01 14:44:08 +0800

    stage2.h

--- project/phase1/bin/structure.h
+++ project/phase1/bin/structure.h
@@ -9,26 +9,41 @@ extern int error_type;
 extern int hasError;
 void yyerror(char *msg);
 
+typedef struct Type{
+    char* type_name;//the name of the type
+    char isStruct;//s -> structure, v-> var, f->function
+    int dim;// deep of the array
+    int hash;//type hash
+    struct Var* contain;
+};
 
-typedef struct {
-    char* name;  //变量名
-    char isStruct;  //v则为变量类型,s则为结构体名，f则为函数
-    char* value;  //变量，函数的type存在这里,结构体这里为结构体名
-    int array_layer;   
-}var;
-// int a, int a[],  struct A{}a [], int funtion a()   project: 
+typedef struct Var{
+    char* name;
+    struct Type* type;
+    struct Type* head, next;
+};
 
-typedef struct {
-    struct var* varPoint;
-    struct varlistNode* next;
-}varlistNode;
+
+// typedef struct {
+//     char* name;  //变量名
+//     char isStruct;  //v则为变量类型,s则为结构体名，f则为函数
+//     char* value;  //变量，函数的type存在这里,结构体这里为结构体名
+//     int array_layer;   
+// }var;
+// // int a, int a[],  struct A{}a [], int funtion a()   project: 
+
+// typedef struct {
+//     struct var* varPoint;
+//     struct varlistNode* next;
+// }varlistNode;
 
 typedef struct Node{
     int line;
     char* name;
     struct Node *head,*next;
     char* value;
-    varlistNode* varList;
+    struct Type* type;
+    struct Var* var;
 };
 
 typedef struct Node* nodePointer;
