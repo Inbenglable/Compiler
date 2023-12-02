209b35f08a3edf2391e460905900427f51d65fa3
radiance0718 <3362985694@qq.com>
2023-12-03 01:09:45 +0800

    resolve function scope

--- project/phase2/test.bat
+++ project/phase2/test.bat
@@ -1,7 +1,9 @@
-for %%I in (01,02,03,04,05,06,07,08,09,10,11,12,13,14,15) do (
-    wsl ./splc ../test/test_2_r%%I.spl > ../test/myout_case%%I.out
+for %%I in (1,2,3,4,5) do (
+    wsl ./bin/splc ./test/test_12112421_%%I.spl > ./test/test_12112421_%%I.out
+    wsl ./bin/splc ./test/test_12112822_%%I.spl > ./test/test_12112822_%%I.out
+    wsl ./bin/splc ./test/test_12112620_%%I.spl > ./test/test_12112620_%%I.out
 )
 
 for %%I in (1,2,3,4,5,6,7) do (
-    wsl ./splc ../test-ex/test_%%I.spl > ../test-ex/test_%%I.out
+    wsl ./bin/splc ./test-ex/test_%%I.spl > ./test-ex/test_%%I.out
 )
