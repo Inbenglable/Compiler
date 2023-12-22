0812b380719db01adfcea411932d253d34c605db
FCKon <FCKong@outlook.com>
2023-12-22 23:42:41 +0800

    nothing

--- project/phase3/ir_optimization.h
+++ project/phase3/ir_optimization.h
@@ -3,6 +3,10 @@
 #ifndef __IR_OPTIMIZATION__
 #define __IR_OPTIMIZATION__
 
+struct Code* const_simplif_1(struct Code* code);
+struct Code* const_simplif_2(struct Code* code);
+struct Code* replace_rval(struct Code* code, char* old, char* new);
+
 struct Code* optimize(struct Code* code);
 
 #endif // !__IR_OPTIMIZATION__
