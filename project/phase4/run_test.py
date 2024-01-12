e8393443c2f1257500e3f22333d7029b778b507d
FCKon <FCKong@outlook.com>
2024-01-12 18:57:18 +0800

    I believe that this test script is robust (恼)

--- project/phase4/run_test.py
+++ project/phase4/run_test.py
@@ -1,13 +1,19 @@
 import os
 
 
-TEST_PATH = './test'
+TEST_PATH = './test/'
 
 
 if __name__ == '__main__':
-    os.system('wsl make')
+    result = os.popen('wsl make')
+    print(result.read())
     files = os.listdir(TEST_PATH)
+    for file in files:
+        if file.endswith('.ir'):
+            os.remove(os.path.join(TEST_PATH, file))
     for file in files:
         if file.endswith('.spl'):
             print('Compiling ' + file)
-            os.system('wsl ./bin/splc ' + os.path.join(TEST_PATH, file))
+            result = os.system('wsl ./bin/splc ' + os.path.join(TEST_PATH, file))
+            print(result)
+    os.system('wsl make clean')
