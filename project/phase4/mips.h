574aae177afb2c59d9700f8a0ad001fa12a6ad20
radiance0718 <3362985694@qq.com>
2024-01-12 21:52:02 +0800

    change mips

--- project/phase4/mips.h
+++ project/phase4/mips.h
@@ -7,17 +7,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include "mips32.h"
-#include "tac.h"
 
-struct Regs{
+
+typedef struct Var{
     char* name;
     int incash;
-    struct VarDesc var;
-}regs[1145];
+    int reg;
+} Var;
+Var vars[1145];
 
+typedef struct Reg{
+    int reg;
+    int var_id;
+    int visited;
+    int preserved; //preserved==1 -> cannot be used by variables
+    //preserved registers: 0, 1, 2, 3, 26, 27, 28, 29, 30, 31
+} Reg;
+Reg regs[32];
 
-Register get_reg(char *reg_name);
+typedef struct Mips{
+    char* op;
+    char* tk_d;
+    char* tk_s;
+    char* tk_t;
+    struct Mips* next;
+    struct Mips* prev;
+} Mips;
+
+typedef struct ret_struct{
+    int reg;
+    struct Mips* code;
+} ret_struct;
+
+ret_struct get_reg(char *var_name);
 
 
 #endif // MIPS_H
