27ab635330d51f691ff6063f0d89393f6ca9f0a0
FCKon <FCKong@outlook.com>
2024-01-13 20:56:50 +0800

    nothing

--- project/phase4/sample/fun_call.s
+++ project/phase4/sample/fun_call.s
@@ -5,11 +5,34 @@
 .text
     # Main code section
     main:
-        
+        li $t0, 1
+        addi $sp, $sp, -4
+        sw $t0, 0($sp)
+        li $t1, 2
+        addi $sp, $sp, -4
+        sw $t1, 0($sp)
         # Exit the program
         li $v0, 10
         syscall
     
     # Function to add two numbers
     add_numbers:
-        
+        # Save the return address
+        addi $sp, $sp, -4
+        sw $ra, 0($sp)
+        addi $sp, $sp, -4
+        sw $fp, 0($sp)
+        # Add the numbers
+        lw $t0, 4($sp)
+        lw $t1, 8($sp)
+        add $v0, $t0, $t1
+        # Restore the arguments
+        addi $sp, $sp, 4
+        lw $a1, 0($sp)
+        addi $sp, $sp, 4
+        lw $a0, 0($sp)
+        # Restore the return address
+        lw $ra, 0($sp)
+        addi $sp, $sp, 4
+        # Return
+        jr $ra
